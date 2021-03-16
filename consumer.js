96463faffa847a15ab657822be668a52a9361c32
Dallin Packer <dallin.packer@aggiemail.usu.edu>
2021-03-15 20:32:52 -0600

    request deletion

--- consumer.js
+++ consumer.js
@@ -14,6 +14,36 @@ s3.listObjects(bucketParams, function(err, data) {
         console.log("Error", err);
     } else {
         let requests = data.Contents;
-        console.log(requests);
+        requests.sort((a, b) => (a.Key > b.Key) ? 1 : -1);
+        if (requests.length > 0) {
+            let request = requests.shift();
+            console.log('request found: ' + request.key);
+            deleteRequest(request);
+        } else {
+            console.log('no requests found');
+            sleep(100);
+        }
     }
 });
+
+function deleteRequest(request) {
+    let params = {
+        Bucket: bucketName,
+        Key: request.Key
+    };
+    s3.deleteObject(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+        } else {
+            console.log('request ' + request.Key + ' removed from ' + bucketName);
+        }
+    });
+}
+
+function sleep(milliseconds) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+        currentDate = Date.now();
+    } while (currentDate - date < milliseconds);
+}
